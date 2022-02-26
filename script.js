7ba6d757986afbaa25a3312504497c82041ff1c6
Luiz de Lima <luiz12359@gmail.com>
2022-02-26 02:48:30 -0300

    projeto navegação rotativa

--- script.js
+++ script.js
@@ -1,46 +1,11 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const btnOpen = document.querySelector('#open')
+const btnClose = document.querySelector('#close')
+const container = document.querySelector('.container')
 
-let currentActive = 1
-next.addEventListener('click', () => {
-    currentActive++
-
-    if (currentActive > circles.length) {
-        currentActive = circles.length
-    }
-    update()
+btnOpen.addEventListener('click', () => {
+    container.classList.add('show-nav')
 })
-
-prev.addEventListener('click', () => {
-    currentActive--
-
-    if (currentActive < 1) {
-        currentActive = 1
-    }
-    update()
-
+btnClose.addEventListener('click', () => {
+    container.classList.remove('show-nav')
 })
-
-function update() {
-    circles.forEach((circle, index) => {
-        if (index < currentActive) {
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
-    const actives = document.querySelectorAll('.active')
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '% '
-
-    if (currentActive === 1) {
-        prev.disabled = true
-    } else if (currentActive === circles.length) {
-        next.disabled = true
-    } else {
-        prev.disabled = false
-        next.disabled = false
-    }
-}
-
+console.log(btnOpen)
